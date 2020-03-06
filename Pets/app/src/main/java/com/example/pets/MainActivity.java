ca73f7613d3f4e17ec4913e9641f72dbc8296ad7
Diego Enriquez <diegoenriquez21@hotmail.com>
2020-03-06 16:23:00 -0500

    login

--- Pets/app/src/main/java/com/example/pets/MainActivity.java
+++ Pets/app/src/main/java/com/example/pets/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Creating app on device", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "The app has started", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "The app has resume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "The app has pause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "The app has stop", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(this, "The app has restart", Toast.LENGTH_SHORT).show();
     }
 }
