1f25892dec72132e38c3318394a66c222fda2aec
DiegoE14 <diegoenriquez21@hotmail.com>
2020-03-30 15:32:01 -0500

    Mostrar boton de opciones

--- Colors/app/src/main/java/com/example/colors/Palette.java
+++ Colors/app/src/main/java/com/example/colors/Palette.java
@@ -1,8 +1,13 @@
 package com.example.colors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Palette extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class Palette extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        //Code here
+    }
+    // Shown the options menu on the Device
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    // Item actions.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.iteTransparent:
+                Toast.makeText(this, "This color ig going to change to transparent", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.iteSemitransparent:
+                break;
+            case R.id.iteOpaque:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
